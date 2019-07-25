dd4ce268036eb2bd01b1a9fcf4983d95b2083d00
Ehevi <48785655+Ehevi@users.noreply.github.com>
2019-07-25 10:49:48 +0200

    Add files via upload

---
+++ mergesort.cpp
@@ -0,0 +1,65 @@
+#include <iomanip>
+#include <cstdlib>
+#include <time.h>
+#include <iostream>
+using namespace std;
+
+void merge(int t[], int left, int middle, int right) //fcja scalajaca//
+{
+    int p[right+1]; //tablica pomocnicza//
+    int i;
+    int a=left; int b=middle;
+    for(i=left; i<=right; i++)
+    {
+        if((a==middle)||((b<=right)&&(t[a]>t[b])))
+        {
+            p[i]=t[b]; b++;
+        }
+        else
+        {
+            p[i]=t[a]; a++;
+        }
+    }
+    for(i=left; i<=right; i++)
+    {
+        t[i]=p[i];
+    }
+}
+
+void mergeSort(int t[], int left, int right) //glowna fcja sortujaca//
+{
+    int middle=(left+right+1)/2;
+    if(middle-left>1) mergeSort(t, left, middle-1);
+    if(right-middle>0) mergeSort(t, middle, right);
+    merge(t, left, middle, right);
+}
+
+int main() //Program glowny//
+{
+  int i;
+  int N;
+  cout<<"Sortowanie przez scalanie\n"; 
+  cout<<"Rozmiar tablicy: ";
+  cin>>N;
+  int t[N];
+    cout<<"Tablica przed sortowaniem:\n\n";
+
+// Wypelniam tablice d[] liczbami pseudolosowymi i wyswietlam jej zawartosc
+
+  srand((unsigned)time(NULL));
+
+  for(i=0; i<N; i++) t[i]=rand()%100; //liczba pseudolosowa z zakresu 0-100//
+  for(i=0; i<N; i++) cout<<setw(4)<<t[i];
+  cout<<endl;
+
+// Sortowanie
+
+  mergeSort(t, 0,N-1);
+  
+// Wynik sortowania
+
+  cout<<"Po sortowaniu:\n\n";
+  for(i=0; i<N; i++) cout<<setw(4)<<t[i];
+  cout<<endl;
+  return 0;
+}
