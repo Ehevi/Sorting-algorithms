211f6fca25382df5fe76c3da849dfee8194c4f5e
Ehevi <48785655+Ehevi@users.noreply.github.com>
2019-08-02 11:53:41 +0200

    Add files via upload

--- countingSort.cpp
+++ countingSort.cpp
@@ -4,29 +4,36 @@
 #include <cstdlib>
 using namespace std;
 
-void countingSort(int a[], int n, int k)
+void countingSort(int *arr, int n, int k)
 {
-    int b[n]; int c[k]; int i;
+    int i;
+    int *b=new int[n];
+    int *c=new int[k];
     for(i=0; i<k; i++) c[i]=0; //przygotowanie tablicy c
-    for(i=0; i<n; i++) c[a[i]]++; //zliczanie, ile jest elementow o i-tym kluczu
+    for(i=0; i<n; i++) c[arr[i]]++; //zliczanie, ile jest elementow o i-tym kluczu
     for(i=1; i<k; i++) c[i]+=c[i-1]; //ile jest elementow mniejszych od danego
-    for(i=n-1; i>=0; i--) b[--c[a[i]]]=a[i]; //umiejscowienie na wlasciwym miejscu w tablicy b
-    for(i=0; i<n; i++) a[i]=b[i]; //przepisanie wyniku do tablicy a
+    for(i=0; i<n; i++)
+    b[--c[arr[i]]]=arr[i]; //umiejscowienie na wlasciwym miejscu w tablicy b
+    //na poczatku zmniejszam wartosci tablicy c, bo indeksowanie zaczyna sie od 0
+    //tzn. pierwszy element ma indeks 0 ind.
+    for(i=0; i<n; i++) arr[i]=b[i]; //przepisanie wyniku do tablicy a
+    delete [] b;
+    delete [] c;
 }
 
-int main() //Program glowny//
+int main() //Program g³ówny//
 {
   int i, n, k;
   cout<<"COUNTINGSORT\n"; 
   cout<<"Rozmiar tablicy: ";
   cin>>n;
-  int t[n];
+  int *t=new int[n];
   cout<<"Zakres kluczy: ";
   cin>>k;
   cout<<"(od 0 do "<<k-1<<")"<<endl;
     cout<<"Tablica przed sortowaniem:\n\n";
 
-// Wypelniam tablice t[] liczbami pseudolosowymi i wyswietlam jej zawartosc
+// Wype³niam tablicê t[] liczbami pseudolosowymi i wyœwietlam jej zawartoœæ
 
   srand((unsigned)time(NULL));
 
@@ -43,5 +50,6 @@ int main() //Program glowny//
   cout<<"Po sortowaniu:\n\n";
   for(i=0; i<n; i++) cout<<setw(4)<<t[i];
   cout<<endl;
+  delete [] t;
   return 0;
 }
