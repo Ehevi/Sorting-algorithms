e62b7886bac65e4c1741afa94cb665df55dd40a2
Ehevi <48785655+Ehevi@users.noreply.github.com>
2019-08-05 20:16:48 +0200

    Rename bubbleSortV1.cpp to bubbleSortVersion1.cpp

--- bubbleSortVersion1.cpp
+++ bubbleSortVersion1.cpp
@@ -18,7 +18,7 @@ void bubbleSort(int arr[], int n)
     }
 }
 
-int main() //Program g≥Ûwny//
+int main() //Program g≈Ç√≥wny//
 {
   int i;
   int size;
@@ -28,7 +28,7 @@ int main() //Program g≥Ûwny//
   int t[size];
 cout<<"Tablica przed sortowaniem:\n\n";
 
-// Wype≥niam tablicÍ t[] liczbami pseudolosowymi i wyúwietlam jej zawartoúÊ
+// Wype≈Çniam tablicƒô t[] liczbami pseudolosowymi i wy≈õwietlam jej zawarto≈õƒá
 
   srand((unsigned)time(NULL));
 
